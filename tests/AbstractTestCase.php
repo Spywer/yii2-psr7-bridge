cc1caa13e049c89b02a31cf02e99cbcae2443b33
spywer <spywer@yandex.ru>
2025-11-28 23:45:54 +0300

    Fix tests

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -16,6 +16,14 @@ class AbstractTestCase extends TestCase
 
     public function setUp(): void
     {
+        // Set required environment variables for Application
+        if (!getenv('YII_ALIAS_WEBROOT')) {
+            putenv('YII_ALIAS_WEBROOT=' . dirname(__DIR__));
+        }
+        if (!getenv('YII_ALIAS_WEB')) {
+            putenv('YII_ALIAS_WEB=http://127.0.0.1:8080');
+        }
+        
         $this->config = include __DIR__ . '/bootstrap.php';
         $this->app = new Application($this->config);
         $this->assertInstanceOf('\yii\Psr7\web\Application', $this->app);
